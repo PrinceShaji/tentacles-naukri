2633b3d3c181199eb5066048fb14928031b442f8
PrinceShaji <22243569+PrinceShaji@users.noreply.github.com>
2019-12-02 10:03:11 +0530

    Added tentacles_settings to public release.

---
+++ tentacles_settings.py
@@ -0,0 +1,14 @@
+# Headers and query parameters. 
+
+headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0",
+           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,/;q=0.8",
+           "Accept-Language": "en-US,en;q=0.5",
+           "Accept-Encoding": "gzip, deflate",
+           "Referer": "https://www.naukri.com/jobs-in-{}",
+           "Content-Type": "application/x-www-form-urlencoded",
+           "DNT": "1",
+           "Connection": "close",
+           "Upgrade-Insecure-Requests": "1"}
+
+query_parameters = 'ql={}%2C&qo=7&qco[]=7'
+
